e615fb907766c823e06046b113ef88995748cff2
Jason Summers <jason1@pobox.com>
2011-08-09 19:09:39 -0400

    Increased version number to 1.4.2.

--- tweakpng.h
+++ tweakpng.h
@@ -26,7 +26,7 @@
 #define SYM_RDQUO       "\""
 #endif
 
-#define TWEAKPNG_VER_STRING      _T("1.4.1")
+#define TWEAKPNG_VER_STRING      _T("1.4.2")
 #define TWEAKPNG_COPYRIGHT_DATE  _T("1999") SYM_ENDASH _T("2011")
 #define TWEAKPNG_HOMEPAGE        _T("http://entropymine.com/jason/tweakpng/")
 
